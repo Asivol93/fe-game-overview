3c28e6230e36660a3891eda800cec3855e6e0599
Lovisa Carling <lovisa.carling@gmail.com>
2022-07-23 23:14:27 +0200

    installed dependecies

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import Form, { FormControl, FormGroup, FormLabel } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
 
 function SignIn() {
   type Inputs = {
@@ -15,8 +17,20 @@ function SignIn() {
   } = useForm<Inputs>()
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input></input>
+    <form>
+      <FormGroup>
+        <FormLabel>Username</FormLabel>
+        <FormControl type='input' placeholder='username...' />
+      </FormGroup>
+      <FormGroup>
+        <FormLabel>Email</FormLabel>
+        <FormControl type='email' placeholder='email@example.com...' />
+      </FormGroup>
+      <FormGroup>
+        <FormLabel>Password</FormLabel>
+        <FormControl type='password' placeholder='password...' />
+      </FormGroup>
+      <Button variant='primary'>Sign In</Button>
     </form>
   )
 }
